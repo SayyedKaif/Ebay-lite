a8244d2024e7e94a8a901b1870bc8be1ac0425b9
Kaif Sayyed <127588575+SayyedKaif@users.noreply.github.com>
2023-03-20 10:17:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+if (document.getElementById('gh-ac').value.length !== 0) { document.getElementById('gh-ac-box').classList.add('gh-ac-box-focus'); document.getElementById('gh').classList.add('gh-sch-focus'); } 
+
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
